0512b6938c5ac7c0b88e8820cd66175c23bb9db6
AlexandruC5 <36231161+AlexandruC5@users.noreply.github.com>
2019-11-20 20:06:35 +0100

    Added ScreenToWorld Method to Render

--- Motor2D/j1Render.h
+++ Motor2D/j1Render.h
@@ -5,6 +5,7 @@
 #include "p2Point.h"
 #include "j1Module.h"
 
+
 class j1Render : public j1Module
 {
 public:
@@ -41,6 +42,8 @@ public:
 	bool DrawQuad(const SDL_Rect& rect, Uint8 r, Uint8 g, Uint8 b, Uint8 a = 255, bool filled = true, bool use_camera = true) const;
 	bool DrawLine(int x1, int y1, int x2, int y2, Uint8 r, Uint8 g, Uint8 b, Uint8 a = 255, bool use_camera = true) const;
 	bool DrawCircle(int x1, int y1, int redius, Uint8 r, Uint8 g, Uint8 b, Uint8 a = 255, bool use_camera = true) const;
+
+	iPoint ScreenToWorld(int x, int y) const;
 
 	// Set background color
 	void SetBackgroundColor(SDL_Color color);
